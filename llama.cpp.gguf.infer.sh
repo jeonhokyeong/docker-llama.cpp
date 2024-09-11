899c2454228243a1e9369723889df0e52d8a88b2
Ubuntu <ubuntu@ip-172-31-13-234.ap-northeast-2.compute.internal>
2024-09-11 11:02:52 +0000

    mod

--- llama.cpp.gguf.infer.sh
+++ llama.cpp.gguf.infer.sh
@@ -1,5 +1,5 @@
 $HOME/llama.cpp/llama-cli \
-	-m $HOME/models/gemma-2-2b-it/gemma-2-2B-it-Q5_K_S.gguf \
+	-m $HOME/models/gemma-2-2b-it/gemma-2-2B-it-BF16.gguf \
 	-p "Please tell me about Docker in 10 sentences." \
     -n 400 \
 	-e \
